bf3a108375c91e67fb47acae3ef8a1681ec3fd0a
jirenguSanyue <562441461@qq.com>
2018-06-27 12:31:33 +0800

    自动更新到对应项时的棋局状态

--- src/index.js
+++ src/index.js
@@ -23,57 +23,46 @@ import './index.css';
     }
   }       //格子组件 Square 不再拥有自身的状态数据了。它从棋盘父组件 Board 接受数据，并且当自己被点击时通知触发父组件改变状态数据，我们称这类的组件为 受控组件。
   */
- function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
- }  //以函数定义的方式重写 Square 组件
-  class Board extends React.Component {
-    constructor() {
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }  //以函数定义的方式重写 Square 组件
+  
+ class Board extends React.Component {
+    /*constructor() {
         super();
         this.state = {
           squares: Array(9).fill(null),
           xIsNext: true,
         };
-    }
+    }*/
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();//用了 .slice() 方法来将之前的数组数据浅拷贝到了一个新的数组中，而不是修改已有的数组。
-        //squares[i] = 'X';
-        //this.setState({squares: squares});
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'x' : 'o';
-        this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
-        }) //每走一步棋，都需要切换 xIsNext 的值以此来实现 X 和 O 轮流落子的效果
-    }
+    
 
     renderSquare(i) {
       return (<Square
-               value={this.state.squares[i]}
-               onClick={()=>this.handleClick(i)}
+               value={this.props.squares[i]}
+               onClick={()=>this.props.onClick(i)}
                />
                );
     }
   
     render() {
-      //const status = 'Next player: X';
-      //const status = 'Next player:' + (this.state.xIsNext ? 'x' : 'o'); //再到 render 方法里添加一点内容来显示当前执子的一方
-      const winner = calculateWinner(this.state.squares);
-      let status;
+      //coconst winner = calculateWinner(this.state.squares);
+      /*let status;
       if (winner) {
         status = 'Winner: ' + winner;
       } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'o');
+      }nst status = 'Next player: X';
+      //const status = 'Next player:' + (this.state.xIsNext ? 'x' : 'o'); //再到 render 方法里添加一点内容来显示当前执子的一方
+      */
       return (
         <div>
-          <div className="status">{status}</div>
+          
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -94,23 +83,104 @@ import './index.css';
     }
   }
   
-  class Game extends React.Component {
-    render() {
-      
+  class Game extends React.Component { //应用状态 state 以及事件处理函数现在都定义在 Game 组件当中。
+    constructor() {
+      super();
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+      };
+    }
 
+    /*handleClick(i) {
+      const squares = this.state.squares.slice();//用了 .slice() 方法来将之前的数组数据浅拷贝到了一个新的数组中，而不是修改已有的数组。
+      //squares[i] = 'X';
+      //this.setState({squares: squares});
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'x' : 'o';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      }) //每走一步棋，都需要切换 xIsNext 的值以此来实现 X 和 O 轮流落子的效果
+    }*/
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares
+        }]),
+        stepNumber: history.length,   //添加 stepNumber: history.length 保证每走一步 stepNumber 会跟着改变
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    jumpTo(step) {      //编写 jumpTo 来切换 stepNumber 的值。
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) ? false : true,
+      });    //修改 xIsNext 来保证对应棋步时，执子的一方是能对应上的。我们可以根据棋步计算出是谁在执子。
+    }
+
+    render() {   //Game 组件的 render 方法现在则要负责获取最近一步的历史记录（当前棋局状态），以及计算出游戏进行的状态（是否有人获胜）。
+      const history = this.state.history;
+      //const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Move #' + move :
+          'Game start';
+        return (
+          <li key={move}>
+            <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+          </li>
+        );   //对于每一步的历史记录，我们都创建了一个带 <a> 链接的 <li> 列表项。为每一个 <a> 添加了一个 jumpTo 方法，用来将棋盘的状态切换至对应的棋步时的状态。
+      });
+
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'o');
+      }
+      
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
     }
   }
+
+  
+  
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
 
   function calculateWinner(squares) {
     const lines = [
@@ -131,12 +201,4 @@ import './index.css';
     }
     return null;
   }
-  
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
   
